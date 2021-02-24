049c4d583471d452ad0fc0852e7bd769cdc22ba4
Moise1 <rwibutso.mo@gmail.com>
2021-02-24 14:51:51 +0200

    refactor the config file

--- server/src/database/config/config.js
+++ server/src/database/config/config.js
@@ -16,7 +16,10 @@ const config= {
   production: {
     use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
-    logging: false
+    logging: false,
+    dialectOptions: {
+      ssl: true,
+    },
   },
 }
 
