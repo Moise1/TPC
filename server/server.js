8d5e5473e2e84a2259ab032b86fbb07b42e267ae
Moise1 <rwibutso.mo@gmail.com>
2021-02-26 10:56:04 +0200

    correct the routes

--- server/server.js
+++ server/server.js
@@ -7,8 +7,8 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(route)
 
-app.use('*', (req, res)=>{
-    return res.status(405).send('Method Not Allowed.');
-})
+// app.use('*', (req, res)=>{
+//     return res.status(405).send('Method Not Allowed.');
+// })
 const port = process.env.PORT || 5000
 app.listen(port , ()=> console.log(`App running on port ${port}`))
